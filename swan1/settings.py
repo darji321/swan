ccf9ecbb5a8dc58dff0b8bd986e31928d1506363
Nihar Darji <65672242+darji321@users.noreply.github.com>
2020-09-16 15:51:19 +0530

    Update settings.py
    Add user id and password...

--- swan1/settings.py
+++ swan1/settings.py
@@ -144,5 +144,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'darjinihar333@gmail.com'
-EMAIL_HOST_PASSWORD = 'cpmiuminhhuujdyg'
+EMAIL_HOST_PASSWORD = '************'
 EMAIL_USE_TLS=True
